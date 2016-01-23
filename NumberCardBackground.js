fcdfbc7dedf67aea540625afa95bf44f6bff4627
Albert Sheu <albert.sheu@gmail.com>
2016-01-23 15:50:30 -0800

    Add Background to number cards

---
+++ NumberCardBackground.js
@@ -0,0 +1,51 @@
+/**
+ * @providesModule NumberCardBackground
+ */
+
+'use strict';
+
+let React = require('react-native');
+let {
+  StyleSheet,
+  Text,
+  View,
+  Animated,
+  PanResponder
+} = React;
+
+let {Dimensions, BaseStyles, Colors} = require('Constants');
+
+
+class NumberCardBackground extends React.Component {
+
+  render() {
+    return (
+      <View style={[
+          BaseStyles.centerContent,
+          styles.card,
+          this.props.isHover ? styles.hover : null,
+          this.props.isDragging ? styles.dragging : null
+        ]}
+        ref="card"
+        >
+        <Text style={BaseStyles.hugeText}>
+          {this.props.combinedTo ? '' : this.props.number}
+        </Text>
+      </View>
+    );
+  }
+
+}
+
+let styles = StyleSheet.create({
+
+  card: {
+    height: Dimensions.cardSide,
+    width: Dimensions.cardSide,
+    backgroundColor: Colors.midBackground,
+    borderRadius: 8,
+    opacity: 0.1
+  }
+});
+
+module.exports = NumberCardBackground;
