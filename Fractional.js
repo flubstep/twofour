77d15e335cff22c04d667d0d0f4c258779f8d00d
Albert Sheu <albert.sheu@gmail.com>
2016-01-23 15:51:15 -0800

    Add Fractional object

---
+++ Fractional.js
@@ -0,0 +1,77 @@
+/**
+ * @providesModule Fractional
+ */
+
+'use strict';
+
+let React = require('react-native');
+let {
+  Dimensions
+} = React;
+
+let {
+  min,
+  max
+} = require('lodash');
+
+
+function lcm(a, b) {
+  let d = gcd(a, b);
+  return (a * b) / d;
+}
+
+
+function gcd(a, b) {
+  let numbers = [Math.abs(a), Math.abs(b)];
+  let larger = max(numbers);
+  let smaller = min(numbers);
+  if (smaller == 0) {
+    return larger;
+  } else {
+    return gcd(smaller, larger % smaller);
+  }
+}
+
+
+class Fractional {
+
+  constructor(numerator, denominator = 1) {
+    console.log('create', numerator, denominator);
+    let divisor = gcd(numerator, denominator);
+    this.numerator = numerator / divisor;
+    this.denominator = denominator / divisor;
+  }
+
+  add(rhs) {
+    let denominator = lcm(this.denominator, rhs.denominator);
+    let lhsNumerator = (this.denominator / denominator) * this.numerator;
+    let rhsNumerator = (rhs.denominator / denominator) * rhs.numerator;
+    return new Fractional(lhsNumerator + rhsNumerator, denominator);
+  }
+
+  subtract(rhs) {
+    let denominator = lcm(this.denominator, rhs.denominator);
+    let lhsNumerator = (this.denominator / denominator) * this.numerator;
+    let rhsNumerator = (rhs.denominator / denominator) * rhs.numerator;
+    return new Fractional(lhsNumerator - rhsNumerator, denominator);
+  }
+
+  multiply(rhs) {
+    return new Fractional(this.numerator * rhs.numerator, this.denominator * rhs.denominator);
+  }
+
+  divide(rhs) {
+    return new Fractional(this.numerator * rhs.denominator, this.denominator * rhs.numerator);
+  }
+
+  toString() {
+    if (this.denominator == 1) {
+      return this.numerator.toString();
+    } else {
+      return this.numerator.toString() + '/' + this.denominator.toString();
+    }
+  }
+
+}
+
+module.exports = Fractional;
